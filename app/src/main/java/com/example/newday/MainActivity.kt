b6e26a9402b8dae5e7944b0d9e9424f15817bc20
Roman <74274722+Ro-JA@users.noreply.github.com>
2023-07-28 22:19:40 +0900

    not work

--- app/src/main/java/com/example/newday/MainActivity.kt
+++ app/src/main/java/com/example/newday/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.newday
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
@@ -10,6 +11,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.newday.ui.theme.NewDayTheme
 
@@ -32,8 +34,8 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Column() {
-
+    Column(modifier = Modifier.fillMaxSize()) {
+Image(painter = painterResource(id = androidx.core.R.drawable.notification_icon_background), contentDescription = null)
         Text(
             text = "Hello $name!",
             modifier = modifier
